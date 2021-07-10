5d416dc1b77095aca862f1198e8f4b98b36b78bb
Aagam Jain <aagamjain50000@gmail.com>
2021-07-11 02:33:05 +0530

    'darktheme'

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const txt = document.getElementById("txt")
+const dark = document.getElementById('dark')
+const calc = document.getElementById("calc")
 
 function dis(val) {
     txt.value += val
@@ -17,4 +19,8 @@ function solve() {
 function backspace() {
     aa = txt.value.slice(0, -1)
     txt.value = aa;
+}
+
+function darkness() {
+    calc.classList.toggle("dark-theme");
 }
